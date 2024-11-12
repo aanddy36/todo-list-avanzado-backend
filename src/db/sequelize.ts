88f57060beaddd4d0894311e16dbdbca975b1d94
aanddy36 <anchibro@hotmail.com>
2024-11-12 17:18:44 -0500

    asd

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 const sequelize = new Sequelize('todo_list', process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: "mysql",
   logging: false,
 });
